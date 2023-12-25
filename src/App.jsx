40864e3927513d79104943affce2bba11da55e3d
RAHUL VR <iamrahulvr29@gmail.com>
2023-12-25 11:52:40 +0530

    install vite react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+    <h1 className="text-3xl font-bold">
+      Hello world!
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
